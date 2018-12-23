1dd68f1deaeb1716a9b4072414561fd056217447
unknown <ksonmezoz@outlook.com>
2018-12-23 13:11:15 +0300

    InputScreenController has been added for InputScreen

--- src/searchengine/controller/InputScreenController.java
+++ src/searchengine/controller/InputScreenController.java
@@ -1,6 +1,9 @@
 package searchengine.controller;
 
+import searchengine.Engine;
 import searchengine.Sentence;
+import searchengine.view.InputScreen;
+import searchengine.view.OutputScreen;
 import searchengine.view.SearchScreen;
 
 import javax.swing.*;
@@ -13,59 +16,21 @@ public class InputScreenController {
     public InputScreenController(){
 
     }
-    /*
+
     public void addSearchButtonActionListener(){
-        SearchScreen screen = SearchScreen.getScreen();
+        InputScreen screen = InputScreen.getScreen();
 
-        screen.getSearchButton().addActionListener(e -> {
-            sentenceList = new ArrayList<Sentence>();
+        screen.getCreateButton().addActionListener(e -> {
 
-            outputPanel.removeAll(); // Cikti ekrani temizlenir
+            OutputScreenController outputController = new OutputScreenController();
+            outputController.clearOutputScreen(); // Cikti ekrani temizlenir
 
-            String url = urlField.getText();
-            fullSentence = sentenceField.getText();
-            String [] parts = fullSentence.split("\\."); // Burada noktayı kaldırıyorlar. Onun yerine tüm punctutationlar kaldırılmalı.
-            //System.out.println(String.join(" ", parts));
-            for(int i = 0; i < parts.length; i++) {
-                String[] words = parts[i].split("\\s+"); // cumleyi kelime kelime ayırıyorlar.
-                for (int j = 0; j< words.length; j++) {
-                    // You may want to check for a non-word character before blindly
-                    // performing a replacement
-                    // It may also be necessary to adjust the character class
-                    words[j] = words[j].replaceAll("[^\\w]", "");
-                }
-                Sentence wordiex = new Sentence(words, url);
-                try{
-                    int content_id = dbService.saveFullSentence(wordiex);
-                    dbService.saveShiftedSentences(wordiex.getShiftedSentences(), content_id);
-                }catch(SQLException exception){ //Eğer bu hata yenirse db de eklenemedi tarzından uyarı verilecek. ama sistem çaısmaya devam edecek
-                    exception.printStackTrace();
-                }
-                sentenceList.add(wordiex);
-            }
+            String url = screen.getUrlField().getText();
+            String fullSentence = screen.getSentenceField().getText();
 
+            Engine engine = new Engine();
 
-            for(int i = 0; i < sentenceList.size(); i++) {
-                searchEngine.addSentence(sentenceList.get(i));
-            }
-
-            //searchEngine.listItems();
-            searchEngine.sort();
-            //��kt� sayfas�nda for i�inde print i�lemini buraya yap
-            for(int i = 0; i < searchEngine.getCountSort(); i++) {
-                JTextField text3 = new JTextField();
-                text3.setBackground(Color.gray);
-                text3.setText("Index: " + searchEngine.getIndex(i) + " Url: " + url);
-                text3.setEditable(false);
-
-                outputPanel.add(text3);
-                outputPanel.revalidate();
-                outputPanel.repaint();
-            }
-
-            searchEngine.clearAllFields(); // searcEngine'i countsort ve count degiskenlerinden kurtarırsak buna gerek kalmayabilir gerci arraylistlerini gene temizlememiz gerek o yüzden bu gerekli
-
+            engine.createIndex(url, fullSentence);
         });
     }
-    */
 }
