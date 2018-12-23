d5e2f0e0f1b1eed0e55838100ca94cc512ac196c
unknown <ksonmezoz@outlook.com>
2018-12-23 13:11:54 +0300

    OutputScreenController has been added for OutputScreen

--- src/searchengine/controller/OutputScreenController.java
+++ src/searchengine/controller/OutputScreenController.java
@@ -1,4 +1,33 @@
 package searchengine.controller;
 
+import searchengine.view.OutputScreen;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class OutputScreenController {
+    private OutputScreen screen;
+
+    public OutputScreenController(){
+        screen = OutputScreen.getScreen();
+    }
+
+    public void clearOutputScreen(){
+        screen.getOutputPanel().removeAll();
+    }
+
+    public void addOutputText(String url, String index){
+        JTextField outputText = new JTextField();
+        JPanel outputPanel;
+
+        outputText.setBackground(Color.gray);
+        outputText.setText(index + " =====> " + url);
+        outputText.setEditable(false);
+
+        outputPanel = screen.getOutputPanel();
+
+        outputPanel.add(outputText);
+        outputPanel.revalidate();
+        outputPanel.repaint();
+    }
 }
